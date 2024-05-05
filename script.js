5c77fbd0285842a72e3c5632d41c9423c31861d0
Aniket555555 <aniketrajak849@gmail.com>
2024-05-05 17:19:52 +0530

    3rd commit

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
+const toggleMenu=document.querySelector('.toggle');
+const navigation=document.querySelector('.navigation')
+toggleMenu.onclick=function(){
+    toggleMenu.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+/* js for images in section */
 let text = document.getElementById('text');
 let leaf = document.getElementById('leaf');
 let hill1 = document.getElementById('hill1');
+let hill2 = document.getElementById('hill2');
+let hill3 = document.getElementById('hill3');
 let hill4 = document.getElementById('hill4');
 let hill5 = document.getElementById('hill5');
+let plant = document.getElementById('plant');
+let tree = document.getElementById('tree');
+let header = document.getElementById('header');
 
-window.addEventListener('scroll', () =>{
+window.addEventListener('scroll', function(){
     let value = window.scrollY;
 
     text.style.marginTop = value * 2.5 + 'px';
-    leaf.style.top = value * -1.5 + 'px';
+    /* leaf.style.top = value * -1.5 + 'px'; */
     leaf.style.left = value * 1.5 + 'px';
     hill5.style.left = value * 1.5 + 'px';
     hill4.style.left = value * -1.5 + 'px';
-    hill1.style.top = value * 1 + 'px';
-});
+    /* hill1.style.top = value * 1 + 'px';
+    hill2.style.top = value * 1 + 'px';
+    hill3.style.top = value * 1 + 'px';
+    hill4.style.top = value * 1 + 'px';
+    hill5.style.top = value * 1 + 'px';
+    plant.style.top = value * 1 + 'px';
+    tree.style.top = value * 1 + 'px';
+    text.style.top = value * 1 + 'px'; */
 
-// Calculate the total height up to the sec section
-const sectionSec = document.querySelector('.sec');
-const totalHeightUpToSec = sectionSec.offsetTop + sectionSec.offsetHeight;
-
-// Set the body height to the calculated total height
-document.body.style.height = totalHeightUpToSec + 'px';
-
-// Prevent scrolling beyond the sec section
-window.addEventListener('scroll', () => {
-    if (window.scrollY > totalHeightUpToSec - window.innerHeight) {
-        // Scroll to the bottom of the sec section if the user tries to scroll further
-        window.scrollTo(0, totalHeightUpToSec - window.innerHeight);
-    }
+    header.style.top = value *0.5 + 'px';
 });
 
 //login/register 
@@ -52,6 +59,13 @@ btnPopup.addEventListener('click', ()=> {
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
 });
+
+/* js for div .sec */
+Splitting();
+
+setTimeout(() => {
+  document.querySelector(".card").focus();
+},1500);
 
 /* js for div .sec */
 Splitting();
